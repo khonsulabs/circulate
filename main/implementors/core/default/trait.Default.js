ca928e25e44bbe618dacd3598816f48c3531f6a3
kl-botsu <botsu@khonsulabs.com>
2022-02-22 20:35:57 +0000

    Deploying to gh-pages from @ khonsulabs/circulate@44d33293b3ec248e8f1e58b2a6d6527b974fafee 🚀

--- main/implementors/core/default/trait.Default.js
+++ main/implementors/core/default/trait.Default.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["circulate"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.57.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"circulate/struct.Relay.html\" title=\"struct circulate::Relay\">Relay</a>","synthetic":false,"types":["circulate::Relay"]}];
+implementors["circulate"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.58.1/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"circulate/struct.Relay.html\" title=\"struct circulate::Relay\">Relay</a>","synthetic":false,"types":["circulate::Relay"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
